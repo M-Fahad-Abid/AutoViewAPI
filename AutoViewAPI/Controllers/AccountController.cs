7aa98268386ebf94515c91b973d6899508e92cc2
Muhammad Fahad <muhammadfahadbinabid@gmail.com>
2024-09-12 11:17:06 +0500

    Add project files.

---
+++ AutoViewAPI/Controllers/AccountController.cs
@@ -0,0 +1,11 @@
+﻿using AutoViewAPI.Controllers.BaseController;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace AutoViewAPI.Controllers
+{
+    public class AccountController : BaseAPIController
+    {
+    }
+}
